5475512e48c28b67cbd313d68640656191edd23b
Thiago Bignotto <bignotto@gmail.com>
2022-01-13 18:42:21 -0300

    all tests passed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,39 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
 
-import { Header } from '../components/Header';
-import { Task, TasksList } from '../components/TasksList';
-import { TodoInput } from '../components/TodoInput';
+import { Header } from "../components/Header";
+import { Task, TasksList } from "../components/TasksList";
+import { TodoInput } from "../components/TodoInput";
 
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
     //TODO - add new task
+    setTasks((oldTasks) => [
+      ...oldTasks,
+      { id: new Date().getTime(), done: false, title: newTaskTitle },
+    ]);
   }
 
   function handleToggleTaskDone(id: number) {
     //TODO - toggle task done if exists
+    const newTasks = tasks.map((t) => ({ ...t }));
+    const idx = tasks.findIndex((t) => t.id === id);
+
+    if (idx < 0) return;
+
+    newTasks[idx].done
+      ? (newTasks[idx].done = false)
+      : (newTasks[idx].done = true);
+
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+    const newTasks = tasks.filter((t) => t.id !== id);
+    setTasks(newTasks);
   }
 
   return (
@@ -26,18 +42,18 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EBEBEB'
-  }
-})
+    backgroundColor: "#EBEBEB",
+  },
+});
